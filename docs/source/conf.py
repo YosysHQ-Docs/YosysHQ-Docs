22b8bf2bc0389bb6e8d235f40225f0108a7bac88
Krystine Sherwin <93062060+KrystalDelusion@users.noreply.github.com>
2025-04-22 10:52:56 +1200

    Drop main search
    See YosysHQ/furo-ys@fcfbb65

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,6 @@ html_theme_options: dict[str, any] = {}
 
 # this is the root doc, we don't need to provde links back here
 html_theme_options["brand_main_docs"] = None
-html_theme_options["brand_main_search"] = None
 
 # These folders are copied to the documentation's HTML output
 html_static_path = ['_static']
